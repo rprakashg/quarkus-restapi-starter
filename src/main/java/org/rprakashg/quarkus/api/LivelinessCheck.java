db5529585f63d895b2324063f4c917f0ab8726d2
Ram Gopinathan <rprakashg@gmail.com>
2022-02-14 14:37:18 -0800

    added helm chart to install onto kubernetes

---
+++ src/main/java/org/rprakashg/quarkus/api/LivelinessCheck.java
@@ -0,0 +1,13 @@
+package org.rprakashg.quarkus.api;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+public class LivelinessCheck implements HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("alive");
+    }
+}
